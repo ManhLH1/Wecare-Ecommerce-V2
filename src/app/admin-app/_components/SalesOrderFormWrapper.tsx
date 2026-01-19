f590df55c0947925b234b5e534877e7c67cb4339
ManhLH <manhlh1.qn@gmail.com>
2026-01-19 10:29:01 +0700

    1

--- src/app/admin-app/_components/SalesOrderFormWrapper.tsx
+++ src/app/admin-app/_components/SalesOrderFormWrapper.tsx
@@ -41,7 +41,7 @@ function SalesOrderFormWrapper() {
             <div className="admin-app-title">
               {activeForm === 'SO' ? 'Tạo đơn bán chi tiết' : 'Tạo đơn báo giá chi tiết'}
             </div>
-            <span className="admin-app-badge admin-app-badge-version">V1.18</span>
+            <span className="admin-app-badge admin-app-badge-version">V1.19</span>
           </div>
           <div className="admin-app-subtitle">
             {activeForm === 'SO' ? 'Đơn hàng bán hàng' : 'Đơn hàng báo giá'}
